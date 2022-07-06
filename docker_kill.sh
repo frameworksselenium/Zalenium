dc82cf6349968cd996ae03f2ae1418c8a23dceeb
frameworksselenium <frameworksselenium@gmail.com>
2022-07-06 12:20:12 -0400

    change in files

--- docker_kill.sh
+++ docker_kill.sh
@@ -2,5 +2,5 @@
 
 if [ "$( docker container inspect -f '{{.State.Running}}' zalenium )" == "true" ]; then
     sudo docker stop zalenium
-    #sudo docker rm zalenium
+    sudo docker rm zalenium
 fi
